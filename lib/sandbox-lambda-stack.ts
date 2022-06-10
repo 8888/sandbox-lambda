b392f678326e7699361f38281f7528b68bb42b45
8888 <leecostello1@gmail.com>
2022-06-10 16:00:41 -0400

    add a basic bucket and lambda

--- lib/sandbox-lambda-stack.ts
+++ lib/sandbox-lambda-stack.ts
@@ -1,9 +1,37 @@
 import { Stack, StackProps } from 'aws-cdk-lib';
+import { PolicyStatement } from 'aws-cdk-lib/aws-iam';
+import { Code, Function, Runtime } from 'aws-cdk-lib/aws-lambda';
+import { BlockPublicAccess, Bucket } from 'aws-cdk-lib/aws-s3';
 import { Construct } from 'constructs';
 
 export class SandboxLambdaStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
+    const bucket = new Bucket(this, 'SandboxBucet', {
+      blockPublicAccess: BlockPublicAccess.BLOCK_ALL,
+    });
+
+    const handler = new Function(this, 'SandboxFunction', {
+      functionName: 'DevSandbox',
+      runtime: Runtime.NODEJS_16_X,
+      code: Code.fromAsset('resources'),
+      handler: 'sandbox.handler',
+    });
+
+    handler.addToRolePolicy(PolicyStatement.fromJson({
+      "Effect": "Allow",
+      "Action": [
+        "secretsmanager:GetSecretValue",
+        "rds-data:BatchExecuteStatement",
+        "rds-data:BeginTransaction",
+        "rds-data:CommitTransaction",
+        "rds-data:ExecuteStatement",
+        "rds-data:RollbackTransaction"
+      ],
+      "Resource": "*"
+    }));
+
+    bucket.grantReadWrite(handler);
   }
 }
